64b3db28474beb2cd734ce9edc68c1679fca064a
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2023-12-03 22:10:32 +0500

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,11 +1,11 @@
+//strings
 #include <iostream>
-
 using namespace std;
-
 int main () 
 {	
 	string phrase = "Readers Transformational Centre";
 	phrase [0] = "L" ;
+	phrase [1] = "E" ;
 	cout << phrase ;
 	
 	return 0 ;
